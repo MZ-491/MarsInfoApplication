eec65d6285131752d8bfc5e889439186690f59f8
M Zain <zain.sakhi3@gmail.com>
2025-12-02 00:08:31 +0500

    Add files via upload

---
+++ MarsInfoApplication/MarsInfoApplication/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Configuration;
+
+namespace ConsoleApp
+{
+    public class Constant
+    {
+        public const int SpeedOfLight = 300000;
+    }
+    public class Mars
+    {
+        public readonly int DistanceToMars = 0;
+
+        public Mars()
+        {
+            DistanceToMars = Convert.ToInt32(ConfigurationManager.AppSettings["DistanceToMars"]);
+        }
+
+        public int GetInfo()
+        {
+            return DistanceToMars / Constant.SpeedOfLight;
+        }
+
+    }
+
+    public class Program
+    {
+       
+        public static void Main(string[] args)
+        {
+            Mars mars = new Mars();
+            Console.WriteLine($"The Speed of Light form earth to mars is approximately {mars.GetInfo()} Kilometers per Hour");
+
+            Console.ReadKey();
+        }
+    }
+}
